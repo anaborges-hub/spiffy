bc5d91da83d3054304cb59756247e09c4e60e3ea
Ana Borges <78922064+anaborges-hub@users.noreply.github.com>
2023-03-20 14:18:07 +0000

    #4 Homepage fixed, touch up on declarations ffile and few others

--- src/components/Homepage/homepage.tsx
+++ src/components/Homepage/homepage.tsx
@@ -1,15 +1,21 @@
+import React from 'react';
 import styles from './homepage.module.css';
+
+// const closet = require('./closet.png');
 
 function Homepage() {
   return (
     <>
-      <div className="bg">this is a picture</div>
-      <img src="assets/closet.jpg" alt="closet" />
-      <div>
-        <button type="button" className={styles.button}>
-          Get started
-        </button>
-      </div>
+      <section>
+        <div className={styles.bg}>
+          {/* <img src={closet} alt="closet" /> */}
+        </div>
+        <div className={styles.container}>
+          <button type="button" className={styles.stbutton}>
+            Get started
+          </button>
+        </div>
+      </section>
     </>
   );
 }
