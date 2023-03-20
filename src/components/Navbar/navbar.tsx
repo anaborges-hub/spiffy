3dceb8bd73d0d7fc515764b22e42771adf053cf1
Ana Borges <78922064+anaborges-hub@users.noreply.github.com>
2023-03-20 14:22:25 +0000

    fix stash

---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,26 @@
+// import { NavLink } from 'react-router-dom';
+
+type Props = {};
+
+function Navbar() {
+  // const [isSidebarOpen, setSidebarOpen] = React.useState(false);
+
+  return (
+    <>
+      {/* <nav>
+        <ul>
+          <li>
+            <link to="/categories">Category</link>
+          </li>
+          <li>
+            <link to="/pick-a-top">Tops</link>
+          </li>
+          <li>
+            <link to="/closet">Closet</link>
+          </li>
+        </ul>
+      </nav> */}
+    </>
+  );
+}
+export default Navbar;
