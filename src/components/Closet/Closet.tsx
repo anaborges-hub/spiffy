ef3b66da51dd654d77a4da06d0db41bd1d908476
Ana Borges <78922064+anaborges-hub@users.noreply.github.com>
2023-04-03 15:40:11 +0100

    #7 gallery functionality working

--- src/components/Closet/Closet.tsx
+++ src/components/Closet/Closet.tsx
@@ -3,13 +3,13 @@ import Carousel from '../Carousel/Carousel';
 import items from '../../item.json';
 
 import styles from './Closet.module.css';
-import Gallery from '../Gallery/Gallery';
+// import Gallery from '../Gallery/Gallery';
 
 function Closet() {
   return (
     <div className={styles.container}>
-      <Gallery items={items} />
-      {/* <Carousel items={items} /> */}
+      {/* <Gallery items={items} /> */}
+      <Carousel items={items} />
     </div>
   );
 }
