ad2a9f50f0ebb90ef0e47cbd9f15bd3ba2a19dec
Ana Borges <78922064+anaborges-hub@users.noreply.github.com>
2023-04-03 12:02:22 +0100

    #7 something is happening

--- src/components/Closet/Closet.tsx
+++ src/components/Closet/Closet.tsx
@@ -3,11 +3,13 @@ import Carousel from '../Carousel/Carousel';
 import items from '../../item.json';
 
 import styles from './Closet.module.css';
+import Gallery from '../Gallery/Gallery';
 
 function Closet() {
   return (
     <div className={styles.container}>
-      <Carousel items={items} />
+      <Gallery items={items} />
+      {/* <Carousel items={items} /> */}
     </div>
   );
 }
